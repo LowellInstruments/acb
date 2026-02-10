8941282a3582ef96eec31a799b93313f238b52c1
kaz <kaz>
2026-02-10 11:52:56 -0500

    more

--- srv_main_gui.py
+++ srv_main_gui.py
@@ -4,7 +4,7 @@ import sys
 import redis
 import subprocess as sp
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtCore
 from PyQt6.QtCore import QTimer, QProcess
 from PyQt6.QtGui import QIcon
 
@@ -185,7 +185,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.proc_aws = QProcess()
         # self.proc_aws.readyReadStandardOutput.connect(self._cb_aws_out)
         # self.proc_aws.readyReadStandardError.connect(self._cb_aws_err)
-        self.proc_aws.setProcessChannelMode(QProcess.MergedChannels)
+        self.proc_aws.setProcessChannelMode(QProcess.ProcessChannelMode.MergedChannels)
         self.proc_aws.stateChanged.connect(self._cb_aws_state)
         self.proc_aws.start("python3", ["aws.py"])
 
